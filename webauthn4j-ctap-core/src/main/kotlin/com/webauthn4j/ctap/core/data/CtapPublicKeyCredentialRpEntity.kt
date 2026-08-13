62e25d300ec0e37b04127111a44ada831d026df9
Yoshikazu Nojima <mail@ynojima.net>
2026-08-14 01:08:19 +0900

    Document why CtapPublicKeyCredential entity wrappers exist
    Add detailed KDoc explaining why CtapPublicKeyCredentialUserEntity and
    CtapPublicKeyCredentialRpEntity are needed instead of the webauthn4j-core
    types:
    
    - UserEntity: CTAP 2.3 §6.2 requires name/displayName to be omitted
      when UV is not done, but WebAuthn L3 defines them as required
    - RpEntity: CTAP requires non-null id and allows nullable name, which
      differs from WebAuthn's PublicKeyCredentialRpEntity
    
    Also update spec reference URLs to current versions.

--- webauthn4j-ctap-core/src/main/kotlin/com/webauthn4j/ctap/core/data/CtapPublicKeyCredentialRpEntity.kt
+++ webauthn4j-ctap-core/src/main/kotlin/com/webauthn4j/ctap/core/data/CtapPublicKeyCredentialRpEntity.kt
@@ -7,11 +7,16 @@ import java.io.Serializable
 
 
 /**
- * [CtapPublicKeyCredentialRpEntity] is a CTAP variant of [PublicKeyCredentialRpEntity]
- * Not like [PublicKeyCredentialRpEntity], id is non-null, and name is nullable
+ * CTAP variant of [PublicKeyCredentialRpEntity] with non-null id and nullable name.
  *
- * @see [§5.4.2. Relying Party Parameters for Credential Generation](https://www.w3.org/TR/webauthn-1/.dictdef-publickeycredentialrpentity)
- * @see [§5.1. authenticatorMakeCredential (0x01)](https://fidoalliance.org/specs/fido2/fido-client-to-authenticator-protocol-v2.1-rd-20191217.html#authenticatorMakeCredential) */
+ * In CTAP, the RP ID is always present (non-null) in authenticatorMakeCredential requests
+ * and authenticatorCredentialManagement responses, whereas WebAuthn's PublicKeyCredentialRpEntity
+ * treats id as optional. Additionally, CTAP allows name to be nullable (e.g., in credential
+ * management RP enumeration responses where only the id is required).
+ *
+ * @see [§5.4.2. Relying Party Parameters for Credential Generation](https://www.w3.org/TR/webauthn-3/#dictdef-publickeycredentialrpentity)
+ * @see [§6.1 authenticatorMakeCredential](https://fidoalliance.org/specs/fido-v2.3-ps-20260226/fido-client-to-authenticator-protocol-v2.3-ps-20260226.html#authenticatorMakeCredential)
+ */
 class CtapPublicKeyCredentialRpEntity : Serializable {
 
     // ~ Instance fields
